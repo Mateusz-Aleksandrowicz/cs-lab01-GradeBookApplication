f17bdf50f3edff1103ad508a06305fd9d796dc36
mateuszaleksandrowicz <mateusz.aleksandrowicz@microsoft.wsei.edu.pl>
2022-03-04 01:17:41 +0100

    CalculateStatistic override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using GradeBook.Enums;
+
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
@@ -9,7 +12,7 @@ namespace GradeBook.GradeBooks
         }
         public override char GetLetterGrade(double averageGrade)
         {
-            if(Students.Count == 0)
+            if (Students.Count == 0)
             {
                 throw new InvalidOperationException();
             }
@@ -24,5 +27,19 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
     }
 }
+        
